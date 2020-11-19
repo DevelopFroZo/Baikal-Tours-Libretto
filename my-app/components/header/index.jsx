fc5b1a10abae9876c2788945267ffb9f5947623a
zmaxcode <promakc31@gmail.com>
2020-11-19 22:57:55 +0800

    add page of events and header

---
+++ my-app/components/header/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './style.module.scss';
+import { TabMenu } from 'primereact/tabmenu';
+import { useRouter } from 'next/router';
+
+
+const Header = (isAdmin = false, url = 'events') => {
+
+    const router = useRouter();
+
+    const items = [
+        {label: 'События', url: '/events'},
+        {label: 'Панель администратора', url: '/admin'}
+    ]
+
+    return(
+        <header className={`p-p-3 p-shadow-1 ${styles.header}`}>
+            <TabMenu 
+                model={items} 
+                activeItem={items.find(el => el.urlName === url)}
+                onTabChange={(e) => router.push(e.value.url)}
+            />
+        </header>
+    )
+}
+
+export default Header;
--- my-app/pages/index.jsx
+++ my-app/pages/index.jsx
@@ -1,12 +1,37 @@
 import Container from '../components/Container'
-import styles from '../styles/Home.module.css'
+import Header from '../components/header';
+import styles from '../styles/Home.module.scss';
+import { Card } from 'primereact/card';
+import { useRouter } from 'next/router';
 import { Button } from 'primereact/button';
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <Container>
-      hello world
-      <Button icon='pi pi-plus' label='Click me' className='p-mt-3 p-d-block'/>
+      <Header url='events'/>
+
+      <ul className={`m-mt-5 ${styles.eventsList}`}>
+        <li className={styles.eventsItem}>
+          <Card 
+            title='Крутое событие'
+            footer={(
+              <span>
+                <Button 
+                  label='Смотреть' 
+                />
+              </span>
+            )}
+          >
+            <p>
+              Крайне крутое событие для крайне крутых чуваков
+            </p>
+          </Card>
+        </li>
+      </ul>
+
     </Container>
   )
 }
