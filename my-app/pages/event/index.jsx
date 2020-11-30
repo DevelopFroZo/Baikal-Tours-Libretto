7caa6255f167d9b5970da16c940484fc01b0d4f6
zmaxcode <promakc31@gmail.com>
2020-11-30 12:09:22 +0800

    add image in page of event

--- my-app/pages/event/index.jsx
+++ my-app/pages/event/index.jsx
@@ -7,19 +7,25 @@ import { Button } from 'primereact/button';
 import { useRouter } from 'next/router';
 import port from '../../helpers/port';
 
-const Event = ({event}) => {
+const Event = ({ event }) => {
 
     const router = useRouter();
     moment.locale('ru')
 
     return (
         <Container>
-            <Header url='/events'/>
-            <Button 
-                icon='pi pi-arrow-left' 
-                label='Вернуться' 
+            <Header url='/events' />
+            <Button
+                icon='pi pi-arrow-left'
+                label='Вернуться'
                 className='p-button-text p-button-secondary p-mt-5'
                 onClick={() => router.push('/')}
+            />
+            <img
+                alt={event.name}
+                src={`http://localhost:${port}/${event.image_path}`}
+                onError={(e) => e.target.src = '/img/imgError.png'}
+                className={styles.image}
             />
             <h1>{event.name}</h1>
             <span className={styles.description}>{event.description}</span>
@@ -45,11 +51,11 @@ const Event = ({event}) => {
     )
 }
 
-export async function getServerSideProps({query}){
+export async function getServerSideProps({ query }) {
 
     const eventId = query.id;
     const eventResponse = await fetch(`http://localhost:${port}/events/${eventId}`);
-    const {payload: event} = await eventResponse.json();
+    const { payload: event } = await eventResponse.json();
 
     return {
         props: {
